644476395877342541ac29429d7412af0608288c
Alex Rothman <rothmanalex@yahoo.com>
2020-08-16 14:32:44 -0400

    Force CapCase to make lower after first
    Instead of just guaranteeing the first character is capitalized,
    also force subsequent characters in a token to be lowercase.

--- NameTransforms.cs
+++ NameTransforms.cs
@@ -117,7 +117,7 @@ namespace DynamicPInvoke
                 return string.Empty;
             }
 
-            return char.ToUpper(s[0]) + s.Substring(1);
+            return char.ToUpper(s[0]) + s.Substring(1).ToLower();
         }
     }
 }
