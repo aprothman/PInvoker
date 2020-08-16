4317d9aab3a9bc68101f6dfd7239baea2adcfbb8
Alex Rothman <rothmanalex@yahoo.com>
2020-08-16 15:50:24 -0400

    Strong-name signing

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -22,6 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("975bab10-501d-4d83-932f-f6e300f75e2a")]
 
+[assembly: InternalsVisibleTo("PInvokerTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a9b270283348628c41e3c0e111cd073d291ea3cc8d9d495f4627a9148b1f2ef8238cf0e671cc61bc2871d8ebea969d07b6a443da3b591f6764dcb7dc38ed13b8348d7e3b223b867ec80914d15e6db5426c7b62664328d5cc96948343d1c20158841c13e75855fe802ee5e1c154bb16382550dc74d077b95224352004c4ce2eb6")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
